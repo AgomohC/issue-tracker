13a8e46f907857a24024f0792b606ec22f99876e
AgomohC <chinaemerem98@yahoo.com>
2021-10-12 20:17:17 +0100

    finished app

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
+app.use("/", (req, res) => {
+  res.send(" <h1>Issue Tracker</h1>");
+});
 app.use("/api", trackerRoutes);
 app.use(notFound);
 app.use(errorHandler);
