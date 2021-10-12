d721ce34e23892fa7da6a55246964873a2a571dc
AgomohC <chinaemerem98@yahoo.com>
2021-10-12 17:08:25 +0100

    finished error handlers

---
+++ errors/index.js
@@ -0,0 +1,8 @@
+const CustomError = require("./custom-error");
+const BadRequest = require("./bad-request");
+const NotFound = require("./not-found-error");
+module.exports = {
+  CustomError,
+  BadRequest,
+  NotFound,
+};
