aaa1800a95dddca4910e7fc15c979be3aef329c6
AgomohC <chinaemerem98@yahoo.com>
2021-10-12 18:54:40 +0100

    finished get  request handler

--- controllers/issue-handler.js
+++ controllers/issue-handler.js
@@ -1,9 +1,51 @@
 const Issues = require("../models/Issues");
 
 const { StatusCodes } = require("http-status-codes");
-const { BadRequest, NotFound } = require("../errors");
-const getIssues = (req, res) => {
-  res.send("get issues");
+const { BadRequestError, NotFoundError } = require("../errors");
+
+const getIssues = async (req, res) => {
+  const { projectName } = req.params;
+  const {
+    issue_title,
+    issue_text,
+    created_by,
+    open,
+    assigned_to,
+    status_text,
+  } = req.query;
+  let queryObject = {};
+
+  if (issue_title) {
+    queryObject.issue_title = issue_title;
+  }
+  if (issue_text) {
+    queryObject.issue_text = issue_text;
+  }
+  if (created_by) {
+    queryObject.created_by = created_by;
+  }
+  if (assigned_to) {
+    queryObject.assigned_to = assigned_to;
+  }
+  if (status_text) {
+    queryObject.status_text = status_text;
+  }
+  if (open === "false") {
+    queryObject.open = false;
+  } else if (open === "true") {
+    queryObject.open = true;
+  }
+  console.log(queryObject);
+  const existingIssues = await Issues.find({
+    project_name: projectName,
+    ...queryObject,
+  });
+
+  if (existingIssues.length < 1) {
+    throw new NotFoundError(`Project ${projectName} has no existing issues`);
+  }
+  const nbHits = existingIssues.length;
+  res.status(StatusCodes.OK).json({ nbHits, existingIssues });
 };
 const createIssue = async (req, res) => {
   const { projectName } = req.params;
@@ -11,7 +53,7 @@ const createIssue = async (req, res) => {
   const { issue_title, issue_text, created_by, assigned_to, status_text } =
     req.body;
   if (!issue_title || !issue_text || !created_by) {
-    throw new BadRequest("required field(s) missing");
+    throw new BadRequestError("required field(s) missing");
   }
   const newIssue = await Issues.create({
     issue_title,
--- errors/index.js
+++ errors/index.js
@@ -1,8 +1,8 @@
 const CustomError = require("./custom-error");
-const BadRequest = require("./bad-request");
-const NotFound = require("./not-found-error");
+const BadRequestError = require("./bad-request");
+const NotFoundError = require("./not-found-error");
 module.exports = {
   CustomError,
-  BadRequest,
-  NotFound,
+  BadRequestError,
+  NotFoundError,
 };
