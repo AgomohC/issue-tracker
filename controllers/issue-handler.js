ad7582a7a54524349eb16511ae8ee95f6447b23b
AgomohC <chinaemerem98@yahoo.com>
2021-10-12 20:10:32 +0100

    finished put request handler

--- controllers/issue-handler.js
+++ controllers/issue-handler.js
@@ -66,8 +66,66 @@ const createIssue = async (req, res) => {
   });
   res.status(StatusCodes.CREATED).json({ newIssue });
 };
-const updateIssue = (req, res) => {
-  res.send("get issues");
+const updateIssue = async (req, res) => {
+  const {
+    _id,
+    issue_title,
+    issue_text,
+    created_by,
+    assigned_to,
+    status_text,
+    open,
+  } = req.body;
+  if (!_id) {
+    throw new BadRequestError(`missing _id`);
+  }
+  if (
+    !issue_title &&
+    !issue_text &&
+    !created_by &&
+    !assigned_to &&
+    !status_text &&
+    !open
+  ) {
+    throw new BadRequestError(`no update field(s) sent, _id: ${_id}`);
+  }
+  let updateObject = {};
+
+  if (issue_title) {
+    updateObject.issue_title = issue_title;
+  }
+  if (issue_text) {
+    updateObject.issue_text = issue_text;
+  }
+  if (created_by) {
+    updateObject.created_by = created_by;
+  }
+  if (assigned_to) {
+    updateObject.assigned_to = assigned_to;
+  }
+  if (status_text) {
+    updateObject.status_text = status_text;
+  }
+  if (open === false) {
+    updateObject.open = false;
+  } else if (open === true) {
+    updateObject.open = true;
+  }
+  console.log(updateObject);
+  const updatedIssue = await Issues.findOneAndUpdate(
+    { _id, _id },
+    updateObject,
+    {
+      new: true,
+    }
+  );
+  if (!updatedIssue) {
+    throw new NotFoundError(`No issue with _id: ${_id}`);
+  }
+  console.log(updatedIssue);
+  res
+    .status(StatusCodes.OK)
+    .json({ result: `successfully updated, _id: ${_id}` });
 };
 const deleteIssue = async (req, res) => {
   const { _id } = req.body;
