da561e6aa9461bd526a73429105687b8f10c023d
AgomohC <chinaemerem98@yahoo.com>
2021-10-13 01:24:34 +0100

    fixed some bugs

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(helmet());
-app.use("/", (req, res) => {
-  res.send(" <h1>Issue Tracker</h1>");
-});
+
 app.use("/api", trackerRoutes);
 app.use(notFound);
 app.use(errorHandler);
